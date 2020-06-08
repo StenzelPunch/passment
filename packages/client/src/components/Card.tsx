88811446ad775d147bdb9cc923157c45e5bae7d4
Stenzel <stenzel.punch@gmail.com>
2020-06-08 12:47:55 +0300

    add card component

---
+++ packages/client/src/components/Card.tsx
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import styled from "@emotion/styled/macro";
+import css from "@emotion/css/macro";
+
+export const cardMaxWidthCss = css`
+  width: 100%;
+  max-width: 300px;
+`;
+
+export const Card = styled.div`
+  background: #ffffff;
+  border-radius: 20px;
+  box-shadow: 0px 8px 20px rgba(0, 174, 255, 0.5);
+  box-sizing: border-box;
+  padding: 30px;
+`;
